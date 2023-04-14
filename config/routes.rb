b23052cd1af13d58f2d0b08933355d76552dca45
Carlos <awocarlos55@gmail.com>
2023-04-14 21:48:55 +0200

    first commit with devise installed

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
+
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end

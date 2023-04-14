875bb196d5b00d061a108f09971f40a2d44b1e19
Carlos <awocarlos55@gmail.com>
2023-04-14 22:20:37 +0200

    make some comfig

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    invitations: 'users/invitations'    }, path: '/users', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
